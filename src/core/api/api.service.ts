889403764fbf3fe68c9f4e706058a0a9d8a28b17
stu2301681017 <stu2301681017@uni-plovdiv.bg>
2025-10-27 14:57:23 +0200

    improved error messaging

--- src/core/api/api.service.ts
+++ src/core/api/api.service.ts
@@ -1,13 +1,23 @@
-import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse, HttpStatusCode } from '@angular/common/http';
+import { ErrorHandler, Injectable } from '@angular/core';
 import { catchError, map, Observable } from 'rxjs';
 import { ApiRoute } from './api.routes';
+import _ from 'lodash';
 
 export interface ApiResponse<T> {
     data: T,
     status: number,
     response: string,
     success: boolean
+}
+
+const defaultErrorHandler = {
+    500: (response: string) => new Error("The server responded with an error. ("+response+")"),
+    501: (response: string) => new Error("The server is currently unresponsive. Try again later. ("+response+")"),
+    400: (response: string) => new Error("The client has sent a bad request. ("+response+")"),
+    403: (response: string) => new Error("You do not have permission."),
+    404: (response: string) => new Error("The requested resource has not been found."),
+    default: (response: string) => new Error("An error has occured."),
 }
 
 @Injectable({
@@ -21,28 +31,40 @@ export class ApiService {
         private http: HttpClient
     ) {}
 
-    public get<T>(url: ApiRoute, params?: {[key: string]: string}): Observable<T> {
+    public get<T>(url: ApiRoute, params: {[key: string]: string}, errorHandler?: {[key: number]: (response: string) => Error}): Observable<T> {
         return this.http.get<ApiResponse<T>>(params ? Object.keys(params).reduce(((p, c) => p.replace('{'+c+'}', params[c])), this.apiBase + url) : this.apiBase + url)
             .pipe(map(x => x.data))
-            .pipe(catchError(err => {throw new Error(err.error.response)}))
+            .pipe(catchError((err: HttpErrorResponse) => {
+                const handler = _.merge(_.cloneDeep(defaultErrorHandler), errorHandler)
+                throw handler[err.status](err.error.response);
+            }))
     }
 
-    public post<T>(url: ApiRoute, body: any, params?: {[key: string]: string}): Observable<T> {
+    public post<T>(url: ApiRoute, body: any, params: {[key: string]: string}, errorHandler?: {[key: number]: (response: string) => Error}): Observable<T> {
         return this.http.post<ApiResponse<T>>(params ? Object.keys(params).reduce(((p, c) => p.replace('{'+c+'}', params[c])), this.apiBase + url) : this.apiBase + url, body)
             .pipe(map(x => x.data))
-            .pipe(catchError(err => {throw new Error(err.error.response)}))
+            .pipe(catchError((err: HttpErrorResponse) => {
+                const handler = _.merge(_.cloneDeep(defaultErrorHandler), errorHandler)
+                throw handler[err.status](err.error.response);
+            }))
     }
 
-    public delete<T>(url: ApiRoute, params?: {[key: string]: string}): Observable<T> {
+    public delete<T>(url: ApiRoute, params: {[key: string]: string}, errorHandler?: {[key: number]: (response: string) => Error}): Observable<T> {
         return this.http.delete<ApiResponse<T>>(params ? Object.keys(params).reduce(((p, c) => p.replace('{'+c+'}', params[c])), this.apiBase + url) : this.apiBase + url)
             .pipe(map(x => x.data))
-            .pipe(catchError(err => {throw new Error(err.error.response)}))
+            .pipe(catchError((err: HttpErrorResponse) => {
+                const handler = _.merge(_.cloneDeep(defaultErrorHandler), errorHandler)
+                throw handler[err.status](err.error.response);
+            }))
     }
     
-    public put<T>(url: ApiRoute, body: any, params?: {[key: string]: string}): Observable<T> {
+    public put<T>(url: ApiRoute, body: any, params: {[key: string]: string}, errorHandler?: {[key: number]: (response: string) => Error}): Observable<T> {
         return this.http.put<ApiResponse<T>>(params ? Object.keys(params).reduce(((p, c) => p.replace('{'+c+'}', params[c])), this.apiBase + url) : this.apiBase + url, body)
             .pipe(map(x => x.data))
-            .pipe(catchError(err => {throw new Error(err.error.response)}))
+            .pipe(catchError((err: HttpErrorResponse) => {
+                const handler = _.merge(_.cloneDeep(defaultErrorHandler), errorHandler)
+                throw handler[err.status](err.error.response);
+            }))
     }
 
 }
