00962055d00b91507b3c0ad99d079bae351cfaef
bobozee <monimitev@gmail.com>
2025-10-28 21:21:30 +0200

    better loading

--- src/core/loading.ts
+++ src/core/loading.ts
@@ -3,6 +3,7 @@ import { computed, ResourceRef, Signal, WritableSignal } from "@angular/core";
 export interface Loading<T> {
     value: Signal<T | undefined>,
     hasValue: Signal<boolean>,
+    hasEmptyValue: Signal<boolean>,
     hasError: Signal<boolean>,
     isLoading: Signal<boolean>,
     error: Signal<Error | undefined>
@@ -25,9 +26,10 @@ export function loadingSource<T>(): LoadingSource<T> {
 export function loading<T>(value: Signal<T | undefined>, error: Signal<Error | undefined>, isLoading: Signal<boolean>): Loading<T> {
     return {
         value,
-        hasValue: computed(() => value() !== undefined && error() === undefined && isLoading() == false),
+        hasValue: computed(() => value() != null && error() == null && isLoading() == false),
+        hasEmptyValue: computed(() => value() == null && error() == null && isLoading() == false),
         isLoading,
-        hasError: computed(() => error() !== undefined && isLoading() == false),
+        hasError: computed(() => error() != null && isLoading() == false),
         error
     }
 }
